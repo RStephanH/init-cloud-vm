f5cd2ab24785d0b8263229a62bdf60a130a44653
rsh <randrianirinahernandezstephan@gmail.com>
2025-09-20 19:11:11 +0300

    feat: switch argument

--- main.sh
+++ main.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
-set -euo pipfail
+set -euo pipefail
 
 MODULE_DIR="./modules"
+
+#Default : run all in numeric order 
+modules_to_run= ($(ls "$MODULE_DIR" |sort ))
+
+#Parse args 
+for arg  in "$@"; do 
+  case $arg in 
+    --only=*)
+      only_list="${arg#*=}"
+      IFS=',' read -r -a modules_to_run <<< "$only_list"
+      ;;
+    --skip=*)
+      skip_list="${arg#*=}"
+      for skip in $(echo "$skip_list" | tr ',' ' ');do
+        modules_to_run=(${modules_to_run[@]/$skip/})
+      done
+      ;;
+    --dry-run)
+      DRY-RUN=1
+      ;;
+  esac
+done
+
+for module in "${module_to_run[@]}"; do 
+  echo ">> Running module : $module "
+  if [[ -n "${DRY-RUN: -}"]]; then
+    echo "[dry-run] $MODULE_DIR/$module"
+  else
+    bash "$MODULE_DIR/$module"
+  fi
+done
