f88036dab3aa5cebed4aa1d15bfe6da5f56191b3
rsh <randrianirinahernandezstephan@gmail.com>
2025-09-24 13:03:30 +0300

    refactor: Update the path

--- modules/10-get_image.sh
+++ modules/10-get_image.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 URL="https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-IMAGE_DIR="~/temp"
+IMAGE_DIR="./vm_utils"
+mkdir $IMAGE_DIR
 
 pushd $IMAGE_DIR
 wget $URL
