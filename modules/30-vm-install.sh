4ac54990d46496b404b77668595cddb5f20f1403
rsh <randrianirinahernandezstephan@gmail.com>
2025-09-24 23:28:36 +0300

    feat: enable libvirtd

--- modules/30-vm-install.sh
+++ modules/30-vm-install.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-VM_UTILS_DIR="./vm_utils"
-IMAGE="noble-server-cloudimg-amd64.img"
-SEE_ISO=
 
-virt-install \
+echo "Let's enable libvirt before running our vm!"
+sudo systemctl enable libvirtd --now
+
+sudo virt-install \
 --name ubuntu-cloud-vm \
 --memory 2048 \
 --vcpus 1 \
---disk path=$"VM_UTILS_DIR"/noble-server-cloudimg-amd64.img,format=qcow2,bus=virtio \
---disk path=$"VM_UTILS_DIR"/seed.iso,device=cdrom \
+--disk path=vm_utils/noble-server-cloudimg-amd64.img,format=qcow2,bus=virtio \
+--disk path=vm_utils/seed.iso,device=cdrom \
 --import \
---os-type linux \
---os-variant ubuntu22.04 \
+--os-variant ubuntu24.04 \
 --network network=default,model=virtio \
 --noautoconsole \
 --graphics none
